e3ea5b1b1b2f884998e600be8485611a4296232f
Sergen Demir <dmrsrgn@gmail.com>
2023-11-10 10:10:52 -0600

    make a simple working prototype

--- src/app/components/Pomodoro.js
+++ src/app/components/Pomodoro.js
@@ -3,12 +3,44 @@
 import React, { useState, useEffect } from 'react';
 
 const Pomodoro = () => {
-  const [time, setTime] = useState(25);
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
 
-  const startTimer = () => {};
+  const startTimer = () => {
+    setIsActive(true);
+  };
+
+  useEffect(() => {
+    let timer;
+
+    if (isActive) {
+      timer = setInterval(() => {
+        if (seconds === 0) {
+          if (minutes === 0) {
+            clearInterval(timer);
+            setIsActive(false);
+            // add additional logic for what happens when the timer reaches 0
+          } else {
+            setMinutes((prevMinutes) => prevMinutes - 1);
+            setSeconds(59);
+          }
+        } else {
+          setSeconds((prevSeconds) => prevSeconds - 1);
+        }
+      }, 1000);
+    } else {
+      clearInterval(timer);
+    }
+
+    return () => clearInterval(timer);
+  }, [isActive, minutes, seconds]);
   return (
     <div className="bg-red-100 p-4 flex flex-col items-center">
-      <span>{time}</span>
+      <span>
+        {String(minutes).padStart(2, '0')}:
+        {String(seconds).padStart(2, '0')}
+      </span>
       <button
         className="bg-red-200 text-white p-2"
         onClick={() => startTimer()}
