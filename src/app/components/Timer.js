05423425e72172e9ef2c801fa0b79ed21108eb57
Sergen Demir <dmrsrgn@gmail.com>
2023-11-11 07:23:29 -0600

    update page.js

--- src/app/components/Timer.js
+++ src/app/components/Timer.js
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 
-const Pomodoro = () => {
+const Timer = () => {
   const [minutes, setMinutes] = useState(25);
   const [seconds, setSeconds] = useState(0);
   const [isActive, setIsActive] = useState(false);
@@ -18,6 +18,7 @@ const Pomodoro = () => {
       timer = setInterval(() => {
         if (seconds === 0) {
           if (minutes === 0) {
+            alert("Time's up!");
             clearInterval(timer);
             setIsActive(false);
             // add additional logic for what happens when the timer reaches 0
@@ -36,19 +37,19 @@ const Pomodoro = () => {
     return () => clearInterval(timer);
   }, [isActive, minutes, seconds]);
   return (
-    <div className="bg-red-100 p-4 flex flex-col items-center">
-      <span>
+    <div className="w-2/3 p-8 flex flex-col items-center">
+      <span className="text-8xl">
         {String(minutes).padStart(2, '0')}:
         {String(seconds).padStart(2, '0')}
       </span>
       <button
-        className="bg-red-200 text-white p-2"
+        className="bg-red-200 text-white p-2 font-bold uppercase "
         onClick={() => startTimer()}
       >
-        Start
+        {isActive ? 'Stop' : 'Start'}
       </button>
     </div>
   );
 };
 
-export default Pomodoro;
+export default Timer;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
-import Pomodoro from './components/Pomodoro';
+'use client';
+
+import { useState } from 'react';
+import Timer from './components/Timer';
+import Navigation from './components/Navigation';
 export default function Home() {
+  const [mode, setMode] = useState([
+    'Pomodoro',
+    'Short Break',
+    'Long Break',
+  ]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Pomodoro App</h1>
-      <Pomodoro />
+    <main className="w-full bg-teal-900 text-white mx-auto flex min-h-screen flex-col items-center justify-between p-24">
+      <Navigation mode={mode} setMode={setMode} />
+      <Timer />
     </main>
   );
 }
